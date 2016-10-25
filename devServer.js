50076306666a968556407ba78445527b0aa54764
tomeraz <tomer.raz@tikalk.com>
2016-10-25 19:08:58 +0300

    added webpack setup, webpack hmr, webpack dashboard

--- devServer.js
+++ devServer.js
@@ -1,13 +1,27 @@
 const express = require('express')
-const testServer = require('./server')
+const webpack = require('webpack')
+const config = require('./webpack.config')
+const devMiddleware = require('webpack-dev-middleware')
+const hotMiddleware = require('webpack-hot-middleware')
+const Dashboard = require('webpack-dashboard');
+const DashboardPlugin = require('webpack-dashboard/plugin');
 
 const PORT = 8888
 
 const app = express()
+const compiler = webpack(config)
+const dashboard = new Dashboard()
 
+compiler.apply(new DashboardPlugin(dashboard.setData));
 
-testServer.configure(app)
+app.use(devMiddleware(compiler, {
+  quiet: true,
+  publicPath: config.output.publicPath
+}))
 
+app.use(hotMiddleware(compiler, {
+  log: () => {}
+}))
 
 const server = app.listen(PORT, 'localhost', function(err) {
   if (err) {
